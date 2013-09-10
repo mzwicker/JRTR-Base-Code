a69afd191b76f35e9c5cf0fc0a577a9d976b68bd
Matthias Zwicker <zwicker@iam.unibe.ch>
2013-09-10 17:56:24 +0200

    Added

---
+++ jrtr/src/main/java/jrtr/SWVertexData.java
@@ -0,0 +1,12 @@
+package jrtr;
+
+/**
+ * A simple implementation of {@link VertexData} which does not add any
+ * additional functionality.
+ */
+public class SWVertexData extends VertexData {
+
+	public SWVertexData(int n) {
+		super(n);
+	}
+}
